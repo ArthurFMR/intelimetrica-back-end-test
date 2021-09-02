143bbca8ab7d022e3d5632ac4c87aca275d0d9c7
unknown <arthurcapacitacion@gmail.com>
2021-09-02 00:35:08 -0400

    Added Create connection function

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,20 @@
+import os
+import psycopg2
 
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+LOCAL_DB_CONFIG = {"dbname": "restaurant", "user": "postgres", "password": "Admin2021"}
+
+
+def create_connection():
+    try:
+        conn = None
+        if DATABASE_URL:
+            # Cloud DB connection
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+            return conn
+        # Local DB Connection
+        conn = psycopg2.connect(**LOCAL_DB_CONFIG)
+        return conn
+    except psycopg2.Error as Err:
+        print("Error at create_connection func. | ", str(Err))
